44fa40e5f22ee32f74c05616b9575db0977f22ad
lauren <laurenchristianson@live.com>
2021-03-24 18:18:41 -0500

    updates

--- password.py
+++ password.py
@@ -18,13 +18,13 @@ import random
 
 #-------------------------------------------------------------------------
 #Ask the user how long they would like their new password to be
-password_length = int(input("Enter password length") or 8 ) #default length is 8
+password_length = int(input("Enter password length: ") or 8 ) #default length is 8
 
 #A list if symbols we can use in our password
 symbols_list = ['!', '@', '#', '%', '&', '*']
 
 #Create a list of numbers 0 - 9
-numbers_list = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+numbers_list = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 #Create a list of the 10 least used lowercase letters in the english language
 lowercase_list = ['z', 'x', 'j', 'q', 'k', 'v', 'b', 'g', 'p', 'w']
@@ -33,7 +33,7 @@ lowercase_list = ['z', 'x', 'j', 'q', 'k', 'v', 'b', 'g', 'p', 'w']
 uppercase_list = ['E', 'T', 'A', 'I', 'N', 'O', 'S', 'H', 'R', 'D']
 
 #Join the 4 lists together to form a password character banks
-password_character_bank =  " "
+password_character_bank = symbols_list + numbers_list + lowercase_list + uppercase_list
 
 #We are going to loop over the password character bank and choose a character at random
 #That character will be asdded to our password
@@ -44,7 +44,7 @@ for x in range(password_length):
     password.append(random.choice(password_character_bank))
 
 #print the password out to the user
-print(''.join(password))
+print(''.join(str(password)))
 
 #Bonus
 #TODO: Copy the generated password to the users clip board
