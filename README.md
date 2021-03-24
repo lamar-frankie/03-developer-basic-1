2a01f6a5ba2bcd5759aa29f5559cd5b8155fa88c
lauren <laurenchristianson@live.com>
2021-03-24 02:11:21 +0000

    password generator

--- README.md
+++ README.md
@@ -30,8 +30,11 @@ In this assignment we will be building a password generator in Python
 
 Answer the following questions
 
-0. Does your org have a secure coding standard? Where is it located? Who maintains it?
+0. Does your org have a secure coding standard? Where is it located? Who maintains it?  
+Yes, there is a security team within the technology organization that maintains it. Docs in Confluence
 
-1. Does your org have blacklisted libraries applications or modules? Where can you find this?
+1. Does your org have blacklisted libraries applications or modules? Where can you find this?  
+We are not supposed to download or use any unapproved apps/libraries but I could not find a list. 
 
-2. You are working on a project and want to include new python library. Identify 3 people that could help you determine if the library is safe for use in production systems
+2. You are working on a project and want to include new python library. Identify 3 people that could help you determine if the library is safe for use in production systems  
+My engineering manager/dev lead, our security group
--- password.py
+++ password.py
@@ -5,10 +5,10 @@
 #Program Notes
 #TODO Fill out the program notes
 """
-Aurthor : [Your name here]
-Email   : [Your email address here]
-Purpose : [What does this program do?]
-Date    : [Date of completion]
+Author  : Lauren
+Email   : laurenchristianson@live.com
+Purpose : Random password generator
+Date    : 2021-03-23
 """
 
 
@@ -17,29 +17,29 @@ import random
 
 
 #-------------------------------------------------------------------------
-#TODO: Ask the user how long they would like their new password to be
-password_length = int(input("<prompt user for password length>") or 8 ) #default length is 8
+#Ask the user how long they would like their new password to be
+password_length = int(input("Enter password length") or 8 ) #default length is 8
 
 #A list if symbols we can use in our password
 symbols_list = ['!', '@', '#', '%', '&', '*']
 
-#TODO: Create a list of numbers 0 - 9
+#Create a list of numbers 0 - 9
 numbers_list = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 
-#TODO: Create a list of the 10 least used lowercase letters in the english language
-lowercase_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+#Create a list of the 10 least used lowercase letters in the english language
+lowercase_list = ['z', 'x', 'j', 'q', 'k', 'v', 'b', 'g', 'p', 'w']
 
-#TODO: Create a list of the 10 most use used uppercase letters in the english language
-uppercase_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+#Create a list of the 10 most use used uppercase letters in the english language
+uppercase_list = ['E', 'T', 'A', 'I', 'N', 'O', 'S', 'H', 'R', 'D']
 
-#TODO: Join the 4 lists together to form a password character bank
-password_character_bank = " " 
+#Join the 4 lists together to form a password character banks
+password_character_bank =  " "
 
 #We are going to loop over the password character bank and choose a character at random
-#That character will be added to our password
+#That character will be asdded to our password
 password = []
 
-#TODO: Loop over password character bank and choose a character at random
+#Loop over password character bank and choose a character at random
 for x in range(password_length):
     password.append(random.choice(password_character_bank))
 
